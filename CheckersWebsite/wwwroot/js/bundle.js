05334b14594b9a0b21c3951619612454b63541ba
Abraham Hosch <hosch250@live.com>
2018-04-27 19:53:48 -0500

    Pull out board editor drag/drop into its own file

--- CheckersWebsite/wwwroot/js/bundle.js
+++ CheckersWebsite/wwwroot/js/bundle.js
@@ -618,6 +618,193 @@ function GetTrueCoords(evt) {
 ;
 //# sourceMappingURL=dragdrop.js.map
 /// <reference path="../Scripts/typings/jquery/jquery.d.ts"/>
+// Taken with modifications from http://svg-whiz.com/svg/DragAndDrop.svg
+var BoardEditorSVGRoot = null;
+var BoardEditorTrueCoords = null;
+var BoardEditorGrabPoint = null;
+var BoardEditorDragTarget = null;
+var BoardEditorGrabScreenCoords = null;
+var BoardEditorGrabClientCoords = null;
+function BoardEditorInit() {
+    BoardEditorSVGRoot = $('#board-editor-svg')[0];
+    $(BoardEditorSVGRoot).on('dragover', BoardEditorDrag);
+    $(BoardEditorSVGRoot).on('dragend', BoardEditorDrop);
+    BoardEditorTrueCoords = BoardEditorSVGRoot.createSVGPoint();
+    BoardEditorGrabPoint = $('.board>svg')[0].createSVGPoint();
+}
+function BoardEditorGrab(evt) {
+    var targetElement = evt.target;
+    BoardEditorGetTrueCoords(evt);
+    if (!BoardEditorDragTarget) {
+        if (!$(targetElement).hasClass('selected-add')) {
+            $('.selected-add').removeClass('selected-add');
+        }
+        $('.selected').removeClass('selected');
+        $('.drag').removeClass('drag');
+        $("#" + targetElement.id).addClass('selected drag');
+        BoardEditorGrabScreenCoords = {
+            x: evt.screenX,
+            y: evt.screenY
+        };
+        BoardEditorGrabClientCoords = {
+            x: evt.clientX,
+            y: evt.clientY
+        };
+        BoardEditorDragTarget = targetElement.closest('g');
+        if (BoardEditorDragTarget) {
+            $('.board svg')[0].appendChild(BoardEditorDragTarget);
+        }
+        targetElement.setAttributeNS(null, 'pointer-events', 'none');
+        BoardEditorGrabPoint.x = BoardEditorTrueCoords.x;
+        BoardEditorGrabPoint.y = BoardEditorTrueCoords.y;
+    }
+}
+;
+function BoardEditorDrag(evt) {
+    if (BoardEditorDragTarget) {
+        BoardEditorGetTrueCoords(evt);
+        var boardBounds = $('.board>svg')[0].getBoundingClientRect();
+        var newX = (BoardEditorTrueCoords.x - BoardEditorGrabPoint.x) / boardBounds.width * 50;
+        var newY = (BoardEditorTrueCoords.y - BoardEditorGrabPoint.y) / boardBounds.height * 50;
+        $(BoardEditorDragTarget)[0].setAttribute('transform', 'translate(' + newX + ',' + newY + ')');
+    }
+}
+;
+function BoardEditorDrop(evt) {
+    $('[pointer-events]').removeAttr('pointer-events');
+    if (BoardEditorDragTarget && $('.selected-add').length === 0) {
+        MovePiece(evt);
+        return;
+    }
+    if ($('.selected-add').length !== 0) {
+        AddPieceToBoard(evt);
+    }
+}
+;
+function MovePiece(evt) {
+    var targetElement = evt.target;
+    if (evt.type === 'dragend') {
+        var dropScreenCoords = {
+            x: evt.screenX,
+            y: evt.screenY
+        };
+        var dropClientCoords = {
+            x: BoardEditorGrabClientCoords.x + (evt.screenX - BoardEditorGrabScreenCoords.x),
+            y: BoardEditorGrabClientCoords.y + (evt.screenY - BoardEditorGrabScreenCoords.y)
+        };
+        var pieceMoved = false;
+        var squares = $('.drop-target');
+        for (var i = 0; i < squares.length; i++) {
+            var el = squares[i];
+            var boundingRect = el.getBoundingClientRect();
+            if (boundingRect.left <= dropClientCoords.x &&
+                boundingRect.right >= dropClientCoords.x &&
+                boundingRect.top <= dropClientCoords.y &&
+                boundingRect.bottom >= dropClientCoords.y) {
+                pieceMoved = true;
+                targetElement = el;
+                var coord = el.id.replace('square', '');
+                var row = parseInt(coord[0]);
+                var col = parseInt(coord[1]);
+                $(BoardEditorDragTarget).removeAttr('transform');
+                $(BoardEditorDragTarget).find('svg').attr('x', $(targetElement).attr('x'));
+                $(BoardEditorDragTarget).find('svg').attr('y', $(targetElement).attr('y'));
+                break;
+            }
+        }
+        if (pieceMoved === false) {
+            $(BoardEditorDragTarget).remove();
+        }
+    }
+    BoardEditorDragTarget = null;
+}
+function AddPieceToBoard(evt) {
+    var dropClientCoords;
+    if (evt.type === 'dragend') {
+        var dropScreenCoords = {
+            x: evt.screenX,
+            y: evt.screenY
+        };
+        dropClientCoords = {
+            x: BoardEditorGrabClientCoords.x + (evt.screenX - BoardEditorGrabScreenCoords.x),
+            y: BoardEditorGrabClientCoords.y + (evt.screenY - BoardEditorGrabScreenCoords.y)
+        };
+    }
+    else {
+        dropClientCoords = {
+            x: evt.clientX,
+            y: evt.clientY
+        };
+    }
+    var squares = $('.drop-target');
+    for (var i = 0; i < squares.length; i++) {
+        var el = squares[i];
+        var boundingRect = el.getBoundingClientRect();
+        if (boundingRect.left <= dropClientCoords.x &&
+            boundingRect.right >= dropClientCoords.x &&
+            boundingRect.top <= dropClientCoords.y &&
+            boundingRect.bottom >= dropClientCoords.y) {
+            var player;
+            var pieceType;
+            switch ($('.selected-add').attr('id')) {
+                case 'black-checker':
+                    player = "Black";
+                    pieceType = "Checker";
+                    break;
+                case 'black-king':
+                    player = "Black";
+                    pieceType = "King";
+                    break;
+                case 'white-checker':
+                    player = "White";
+                    pieceType = "Checker";
+                    break;
+                case 'white-king':
+                    player = "White";
+                    pieceType = "King";
+                    break;
+            }
+            var coord = el.id.replace('square', '');
+            var row = parseInt(coord[0]);
+            var col = parseInt(coord[1]);
+            var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+            var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            svg.setAttribute('id', "svg" + row + col);
+            svg.setAttribute('onclick', "pieceClick(" + row + col + ")");
+            svg.setAttribute('height', '12.5%');
+            svg.setAttribute('width', '12.5%');
+            svg.setAttribute('style', 'fill:none');
+            svg.setAttribute('x', "" + $(el).attr('x'));
+            svg.setAttribute('y', "" + $(el).attr('y'));
+            var image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
+            image.setAttribute('id', "piece" + row + col);
+            image.setAttribute('player', player);
+            image.setAttribute('height', '100%');
+            image.setAttribute('width', '100%');
+            image.setAttribute('href', "/images/SteelTheme/" + player + pieceType + ".png");
+            var rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+            rect.setAttribute('id', "rect" + row + col);
+            rect.setAttribute('class', 'selected-piece-highlight');
+            rect.setAttribute('height', '100%');
+            rect.setAttribute('width', '100%');
+            rect.setAttribute('style', 'fill:none; stroke:goldenrod');
+            svg.appendChild(image);
+            svg.appendChild(rect);
+            g.appendChild(svg);
+            $("#piece" + row + col).closest('g').first().remove();
+            $('.board>svg')[0].appendChild(g);
+            $('.selected-add').removeClass('selected-add');
+            break;
+        }
+    }
+}
+function BoardEditorGetTrueCoords(evt) {
+    BoardEditorTrueCoords.x = evt.clientX;
+    BoardEditorTrueCoords.y = evt.clientY;
+}
+;
+//# sourceMappingURL=boardEditor.js.map
+/// <reference path="../Scripts/typings/jquery/jquery.d.ts"/>
 function getAdjustedIndex(value) {
     switch ($('.board').attr('orientation').toLowerCase()) {
         case "black":
